9f5533ff10194fab86c7b07a6fcb4fffeac9f966
Yoaquim Cintrón <yoaquim@outlook.com>
2014-05-09 01:25:51 -0400

    Fixed CSS not showing up when connected to server. Bit of documentation added; changed module dependencies in package.json: turns out express no longer users body parsers directly, they're separate modules now. Added different banner styles.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,30 @@
+/*
+*  Import required modules
+*/
 var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();
-app.use(bodyParser());
+var morgan = require('morgan');
 
+/*
+*  Create an express server.
+*
+*  Attach 'morgan' object - a logger (log express events).
+*  Might want to remove it, or pipe it to file.
+*
+*  Attach a bodyParser in order to handle json and urlencoded
+*  bodies.
+*
+*  Add the static middleware, which allow express to serve up
+*  static content in the specified directory (for CSS/JS).
+*/
+var app = express();
+app.use(morgan());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+
+/*
+*  Set up routing.
+*/
 
 //Landing/login page
 app.get('/', function(request, response){
@@ -21,6 +43,11 @@ app.get('/opentok', function(request, response){
   response.sendfile(__dirname+'/public/browser_demo.html');
 });
 
+/*
+*  Start the server by listening in on specified port.
+*
+*  (alternative: process.env.PORT, instead of hard-coded port)
+*/
 app.listen(8080, function(){
   console.log("Server is now listening on port 8080.");
 });
