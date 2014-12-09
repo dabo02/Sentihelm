21929541fae2bc526c6c3930a1a167e2ba08f892
Jose F Martinez <jose.martinez70@upr.edu>
2014-12-09 10:17:12 -0400

    Fixed merge errors.

--- public/javascript/sentihelm-ng.js
+++ public/javascript/sentihelm-ng.js
@@ -1430,18 +1430,8 @@
   //Controller for tipfeed route; handles the tip feed
   //which lets you interact with tips, depends heavily
   //on paginatorService
-<<<<<<< HEAD
-<<<<<<< HEAD
-  app.controller('TipFeedController', ['$scope', 'socket', 'ngDialog', 'paginatorService', 'usSpinnerService', '$location', '$anchorScroll', 'Session',
-  function($scope, socket, ngDialog, paginatorService, usSpinnerService, $location, $anchorScroll, Session){
-=======
   app.controller('TipFeedController', ['$scope', '$rootScope','socket', 'ngDialog', 'paginatorService', 'usSpinnerService', '$location', '$anchorScroll',
   function($scope, $rootScope, socket, ngDialog, paginatorService, usSpinnerService, $location, $anchorScroll){
->>>>>>> d058dde73b526376c1d73db4ed9da098327b2496
-=======
-  app.controller('TipFeedController', ['$scope', '$rootScope','socket', 'ngDialog', 'paginatorService', 'usSpinnerService', '$location', '$anchorScroll',
-  function($scope, $rootScope, socket, ngDialog, paginatorService, usSpinnerService, $location, $anchorScroll){
->>>>>>> FETCH_HEAD
 
     //Vars needed for pagination; paginatorSet contains
     //number of total pages, divided by groups of 10
@@ -1626,29 +1616,20 @@
         });
       }
     };
-<<<<<<< HEAD
-<<<<<<< HEAD
-
+    
     socket.on('new-tip', function(data){
       if(data.clientId===Session.clientId){
         tipfeed.counter++;
       }
     });
-    
-=======
-=======
->>>>>>> FETCH_HEAD
-    
+
     tipfeed.filterTips = function() {
       var date = new Date();
       var crimePosition = 1;
       $rootScope.$broadcast('discard-current-tips',[]);
       paginatorService.initializeFeed(date, false, crimePosition);
     };
-<<<<<<< HEAD
->>>>>>> d058dde73b526376c1d73db4ed9da098327b2496
-=======
->>>>>>> FETCH_HEAD
+
   }]);
 
   //Controller for the tip's attachments; must display
