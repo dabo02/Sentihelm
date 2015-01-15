38b2f3a9f4574653ab3724262906897694ca2dff
Victor Martinez <victor.martinez3@upr.edu>
2015-01-15 13:41:00 -0400

    small variable change.

--- public/javascript/sentihelm-mostwanted.js
+++ public/javascript/sentihelm-mostwanted.js
@@ -16,7 +16,7 @@
      * @name wantedPerson
      * @param {ngService} $rootScope provide separation between the model and the view, via a mechanism for watching the model for changes.
      * @returns {ngDirective}
-     * @restrict E
+     * @restrict EA
      * @scope
      * @description custom angular directive used in html as wanted-person
      * */
@@ -28,7 +28,7 @@
                 save: "&",                // for saving changes
                 confirm: "&",             // for confirming deletion
                 fileSelect: "&",          // for selecting image file
-                person: "=",              // the person object for the model, information should be in person.attributes object
+                criminal: "=",              // the person object for the model, information should be in person.attributes object
                 disableNewButton: "=",    // boolean value, disables/enables new button
                 wantedArray: "=",         // array of wanted people
                 editedPeopleIndices: "=", // indices of people being edited
