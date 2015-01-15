fd3ef31de32b8f36183a9731f7b9bfa180c9a65b
Victor Martinez <victor.martinez3@upr.edu>
2015-01-15 13:26:57 -0400

    Added some documentation

--- public/javascript/sentihelm-mostwanted.js
+++ public/javascript/sentihelm-mostwanted.js
@@ -3,21 +3,36 @@
  */
 
 (function (angular, undefined) {
+    /**
+     * @ngdoc Module
+     * @name sh.mostwanted
+     * @requires none
+     * @description Creates the module for the controllers and directives of most wanted
+     * */
     var mostWantedModule = angular.module('sh.mostwanted', []);
 
+    /**
+     * @ngdoc Directive
+     * @name wantedPerson
+     * @param {ngService} $rootScope provide separation between the model and the view, via a mechanism for watching the model for changes.
+     * @returns {ngDirective}
+     * @restrict E
+     * @scope
+     * @description custom angular directive used in html as wanted-person
+     * */
     mostWantedModule.directive('wantedPerson', function ($rootScope) {
         return {
             restrict: 'EA',
             scope: {
-                change: "&",
-                save: "&",
-                confirm: "&",
-                fileSelect: "&",
-                person: "=",
-                disableNewButton: "=",
-                wantedArray: "=",
-                editedPeopleIndices: "=",
-                index: "="
+                change: "&",              // for observing changes in models
+                save: "&",                // for saving changes
+                confirm: "&",             // for confirming deletion
+                fileSelect: "&",          // for selecting image file
+                person: "=",              // the person object for the model, information should be in person.attributes object
+                disableNewButton: "=",    // boolean value, disables/enables new button
+                wantedArray: "=",         // array of wanted people
+                editedPeopleIndices: "=", // indices of people being edited
+                index: "="                // index of current element
             },
             templateUrl: '/wanted-person-template.html',
             transclude: true,
