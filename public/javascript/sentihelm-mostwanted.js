3eaff3a299b1b3726d3042c201b3191e5ddef72b
Victor Martinez <victor.martinez3@upr.edu>
2015-01-16 10:04:45 -0400

    tweak to limitSummary filter.

--- public/javascript/sentihelm-mostwanted.js
+++ public/javascript/sentihelm-mostwanted.js
@@ -21,63 +21,71 @@
      * @description custom angular directive used in html as wanted-person
      * */
         .directive('wantedPerson', function ($rootScope) {
-        return {
-            restrict: 'EA',
-            scope: {
-                change: "&",              // for observing changes in models
-                save: "&",                // for saving changes
-                confirm: "&",             // for confirming deletion
-                fileSelect: "&",          // for selecting image file
-                criminal: "=",            // the person object for the model, information should be in person.attributes object
-                disableNewButton: "=",    // boolean value, disables/enables new button
-                wantedArray: "=",         // array of wanted people
-                editedPeopleIndices: "=", // indices of people being edited
-                index: "="                // index of current element
-            },
-            templateUrl: '/wanted-person-template.html',
-            link: function(scope, element) {
-                // creates boolean value from expression, if true this element was just created and added to the list
-                // by the new button. Otherwise, just normal ngRpeat directive.
-                scope.justCreated = scope.editing = (scope.index === $rootScope.lastPersonAddedIndex);
+            return {
+                restrict: 'EA',
+                scope: {
+                    change: "&",              // for observing changes in models
+                    save: "&",                // for saving changes
+                    confirm: "&",             // for confirming deletion
+                    fileSelect: "&",          // for selecting image file
+                    criminal: "=",            // the person object for the model, information should be in person.attributes object
+                    disableNewButton: "=",    // boolean value, disables/enables new button
+                    wantedArray: "=",         // array of wanted people
+                    editedPeopleIndices: "=", // indices of people being edited
+                    index: "="                // index of current element
+                },
+                templateUrl: '/wanted-person-template.html',
+                link: function (scope, element) {
+                    // creates boolean value from expression, if true this element was just created and added to the list
+                    // by the new button. Otherwise, just normal ngRpeat directive.
+                    scope.justCreated = scope.editing = (scope.index === $rootScope.lastPersonAddedIndex);
 
-                // templates
-                scope.wantedCardTemplate = '/most-wanted-card.html';
-                scope.wantedEditorTemplate = '/most-wanted-editor.html';
+                    // templates
+                    scope.wantedCardTemplate = '/most-wanted-card.html';
+                    scope.wantedEditorTemplate = '/most-wanted-editor.html';
 
-                // toggles state of editing, shows and hides editing form and card respectively.
-                scope.toggleEditing = function() {
-                    scope.editing = !scope.editing;
-                };
+                    // toggles state of editing, shows and hides editing form and card respectively.
+                    scope.toggleEditing = function () {
+                        scope.editing = !scope.editing;
+                    };
 
-                // discards current card and editor for the wanted person
-                // can only be run if the card was just created and not saved
-                // to the database.
-                scope.discard = function() {
-                    scope.toggleEditing();
-                    element.controller().wantedArray.pop();
-                    element.controller().disableNewButton = false;
-                    element[0].remove();
-                };
+                    // discards current card and editor for the wanted person
+                    // can only be run if the card was just created and not saved
+                    // to the database.
+                    scope.discard = function () {
+                        scope.toggleEditing();
+                        element.controller().wantedArray.pop();
+                        element.controller().disableNewButton = false;
+                        element[0].remove();
+                    };
+                }
+
+            };
+        })
+        .filter('limitName', function () {
+            function limitName(inputName) {
+                var nameArray = inputName.split(' ');
+                var name = nameArray[0] + ' ' + nameArray[1];
+                return name;
             }
 
-        };
-    })
-        .filter('limitName', function () {
-        function limitName(inputName) {
-            var nameArray = inputName.split(' ');
-            var name = nameArray[0] + ' ' + nameArray[1];
-            return name;
-        }
-
-        return limitName;
-    })
+            return limitName;
+        })
         .filter('limitSummary', function () {
             function limitSummary(inputSummary) {
                 return inputSummary.split(' ').map(function (word, count) {
-                    if (count < 35) {
-                        return word;
-                    }
-                }).join(' ') + '...';
+                        var theWord = '';
+                        if (count < 35) {
+                            if (count === 0) {
+                                theWord = word[0].toUpperCase() + word.split('').slice(1).join('').toLowerCase();
+                            } else if (count === 34) {
+                                theWord = word + '...';
+                            } else {
+                                theWord = word.toLowerCase();
+                            }
+                            return theWord;
+                        }
+                    }).join(' ');
             }
 
             return limitSummary;
