3655c74bee0ce7e323c677d376bca5042236104f
Victor Martinez <victor.martinez3@upr.edu>
2015-01-26 10:56:30 -0400

    chat stub

---
+++ public/javascript/sentihelm-chat.js
@@ -0,0 +1,10 @@
+/**
+ * Created by marti_000 on 1/26/2015.
+ */
+
+(function (angular, undefined) {
+    angular.module('sh.chat', [])
+        .controller('ChatController', ['$scope', function ($scope) {
+            
+        }]);
+})(window.angular);
