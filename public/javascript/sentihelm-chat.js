00bafa9cf94f442a38dde34658f40e1aa1e75acf
Victor Martinez <victor.martinez3@upr.edu>
2015-01-26 14:19:49 -0400

    message stub

--- public/javascript/sentihelm-chat.js
+++ public/javascript/sentihelm-chat.js
@@ -3,33 +3,55 @@
  */
 
 (function (angular, undefined) {
-    angular.module('sh.chat', [])
-        .factory('messageService', function () {
-            var Message = (function (){
-                function Message() {
+    angular.module('sh.chat', ['btford.socket-io'])
+        .factory('chatSocket', function (socketFactory) {
+            return socketFactory({
+                prefix: 'chat-',
+                ioSocket: io.connect('')  // connect to chat server
+            });
+        })
+        .controller('ChatManagerController', ['chatSocket', function (chatSocket) {
+            var ChatManagerController = this,
+                messages = [];
 
+            /**
+             * @class Message
+             * @param messageData {object} is of format { 'sender': '', 'receiver': '', 'message': '', 'dateTime': 1234567890 }
+             * @return new Instace of message
+             * */
+            var Message = (function () {
+                function Message(messageData) {
+                    this.sender = messageData.sender || null;
+                    this.receiver = messageData.receiver || null;
+                    this.message = messageData.message || null;
+                    this.dateTime = messageData.dateTime || +new Date();
                 }
+
+                Message.prototype.toJSON = function () {
+                    var m = {
+                        sender: this.sender,
+                        receiver: this.receiver,
+                        message: this.message,
+                        dateTime: this.dateTime
+                    };
+
+                    return JSON.stringify(m);
+                };
 
                 return Message;
             })();
 
-            return {
-                Message: Message
-            };
-        })
-        .controller('ChatController', ['$scope', 'messageService', function ($scope) {
-            var self = this;
-
-            this.send = function () {
-
-            };
-
-            this.recieve = function () {
+            this.send = function (message) {
 
             }
 
-            this.save = function () {
+            this.receive = function (message) {
 
             }
+
+            chatSocket.on('chat-new-message', function (message) {
+                ChatManagerController.receive(message);
+            });
+
         }]);
 })(window.angular);
