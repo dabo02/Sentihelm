cd994182b48b82d8cf63554a30f78fced2db468d
Victor Martinez <victor.martinez3@upr.edu>
2015-01-30 20:06:13 -0400

    still has some bugs.

--- public/javascript/sentihelm-chat.js
+++ public/javascript/sentihelm-chat.js
@@ -2,6 +2,38 @@
  * Created by Victor A. Martinez on 1/26/2015.
  */
 
+var Message = (function () {
+    function Message(messageData) {
+        var self = this;
+        this.sender = messageData.sender || Session.clientId;
+        this.receiver = messageData.receiver || null;
+        this.message = messageData.message || null;
+        this.dateTime = messageData.dateTime || +new Date();
+
+        /**
+         * Iterates over all the properties of the message, checking if they're all set.
+         * */
+        for (var item in Object.keys(this)) {
+            if (this.hasOwnProperty(item) && this[item] == null) {
+                throw new Error("Can't create message. Property: " + item + " has not been set.");
+            }
+        }
+    }
+
+    Message.prototype.toJSON = function () {
+        var m = {
+            sender: this.sender,
+            receiver: this.receiver,
+            message: this.message,
+            dateTime: this.dateTime
+        };
+
+        return m;
+    };
+
+    return Message;
+})();
+
 (function (angular, undefined) {
     'use strict';
     angular.module('sh.chat', ['btford.socket-io'])
@@ -57,37 +89,6 @@
              * @throws Error
              * @return new Instance of message
              * */
-            var Message = (function () {
-                function Message(messageData) {
-                    var self = this;
-                    this.sender = messageData.sender || Session.clientId;
-                    this.receiver = messageData.receiver || null;
-                    this.message = messageData.message || null;
-                    this.dateTime = messageData.dateTime || +new Date();
-
-                    /**
-                     * Iterates over all the properties of the message, checking if they're all set.
-                     * */
-                    for (var item in Object.keys(this)) {
-                        if (this.hasOwnProperty(item) && this[item] == null) {
-                            throw new Error("Can't create message. Property: " + item + " has not been set.");
-                        }
-                    }
-                }
-
-                Message.prototype.toJSON = function () {
-                    var m = {
-                        sender: this.sender,
-                        receiver: this.receiver,
-                        message: this.message,
-                        dateTime: this.dateTime
-                    };
-
-                    return m;
-                };
-
-                return Message;
-            })();
 
             this.send = function () {
                 //chatSocket.emit('test', {
