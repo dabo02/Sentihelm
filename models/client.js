e5a09cf11875f893b7a796b383b15a39e9b2ca34
Victor Martinez <victor.martinez3@upr.edu>
2015-03-25 11:53:49 -0400

    added client model stub

---
+++ models/client.js
@@ -0,0 +1,28 @@
+// model for the client table
+
+var db = require('../lib/db');
+var Q = require('q');
+var Client = db.Object.extend('Client');
+
+module.exports.getById = function (clientId) {
+
+  return Q.Promise(function (resolve, reject, notify) {
+    console.log('client is %s', clientId);
+
+    new db.Query(Client)
+      .include('regions')
+      .include('mostWantedList')
+      .get(clientId)
+      .then(function (client) {
+        if (client) {
+          resolve(client);
+        } else {
+          reject(new Errror("Couldn't get client"));
+        }
+
+
+      }, function (error) {
+        reject(new Error(error));
+      });
+  });
+}
